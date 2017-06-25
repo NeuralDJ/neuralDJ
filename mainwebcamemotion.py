d4cd00a83cae26546ad6c8beaf3790352f328ba6
Vikas Mohandoss <vm2446@columbia.edu>
2017-06-24 18:47:04 -0700

    comments for mainemotion

--- mainwebcamemotion.py
+++ mainwebcamemotion.py
@@ -1,4 +1,5 @@
 webcamtoemotion = __import__('webcamtoemotion')
 numofframes = 5
 emotionFeatures = webcamtoemotion.GetEmotionFromCameraFeed(numofframes)
+#emotionFeatues has the mode of the features of (numofframes*numoffacesineachframe)
 print(emotionFeatures)
