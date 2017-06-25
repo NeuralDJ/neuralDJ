ace8d41b23e62ac84dd856a5b05bed34bb824658
tiagosomda <tsomda@gmail.com>
2017-06-24 18:20:01 -0700

    server changes

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import time
 feed = __import__('feed')
 emotion = __import__('emotion')
 
-print ('started... geting frames')
+def run_program():
+   print ('started... geting frames')
+   
+   start_time = time.time()
+   frames = feed.getCameraFrames(1)
+   print ("get_frames - " + str(time.time() - start_time) +  "s")
+   
+   start_time = time.time()
+   encodedFrames = feed.encodeFrames(frames)
+   print ("encode - " + str(time.time() - start_time) +  "s")
+   
+   emotionOutput = emotion.detect_emotion(encodedFrames)
+   print(emotionOutput)
+   print ("emotion - " + str(time.time() - start_time) +  "s")
 
-start_time = time.time()
-frames = feed.getCameraFrames(5)
-print ("get_frames - " + str(time.time() - start_time) +  "s")
-
-start_time = time.time()
-encodedFrames = feed.encodeFrames(frames)
-print ("encode - " + str(time.time() - start_time) +  "s")
-
-emotionOutput = emotion.detect_emotion(encodedFrames)
-print(emotionOutput)
-print ("emotion - " + str(time.time() - start_time) +  "s")
+   return 'rock_and_roll_baby! : ' + str(time.time() - start_time)
