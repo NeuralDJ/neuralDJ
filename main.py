17e12dd3dc50991eae5d9303a35cad181c4f2a42
tiagosomda <tsomda@gmail.com>
2017-06-24 17:00:05 -0700

    added runtime of functions

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import time
 feed = __import__('feed')
 emotion = __import__('emotion')
 
+print ('started... geting frames')
 
+start_time = time.time()
 frames = feed.getCameraFrames(5)
+print ("get_frames - " + str(time.time() - start_time) +  "s")
 
+start_time = time.time()
 encodedFrames = feed.encodeFrames(frames)
+print ("encode - " + str(time.time() - start_time) +  "s")
 
 emotionOutput = emotion.detect_emotion(encodedFrames)
-
 print(emotionOutput)
+print ("emotion - " + str(time.time() - start_time) +  "s")
