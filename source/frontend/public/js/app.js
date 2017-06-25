11f654bfdb0c1ad79ba68c69081e5acd8491e19a
tiagosomda <tsomda@gmail.com>
2017-06-25 01:54:37 -0700

    got actions to spotify working

--- source/frontend/public/js/app.js
+++ source/frontend/public/js/app.js
@@ -1,58 +1,118 @@
 $( document ).ready(function() {
-        /**
-         * Obtains parameters from the hash of the URL
-         * @return Object
-         */
-        function getHashParams() {
-          var hashParams = {};
-          var e, r = /([^&;=]+)=?([^&;]*)/g,
-              q = window.location.hash.substring(1);
-          while ( e = r.exec(q)) {
-             hashParams[e[1]] = decodeURIComponent(e[2]);
-          }
-          return hashParams;
-        }
 
-        var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-            userProfileTemplate = Handlebars.compile(userProfileSource),
-            userProfilePlaceholder = document.getElementById('user-profile');
+  var access_token = undefined;
+  var refresh_token = undefined;
+  var error = undefined;
 
-        var oauthSource = document.getElementById('oauth-template').innerHTML,
-            oauthTemplate = Handlebars.compile(oauthSource),
-            oauthPlaceholder = document.getElementById('oauth');
+  function init() {
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    function getHashParams() {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+      while ( e = r.exec(q)) {
+         hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      return hashParams;
+    }
 
-        var params = getHashParams();
+    var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+        userProfileTemplate = Handlebars.compile(userProfileSource),
+        userProfilePlaceholder = document.getElementById('user-profile');
+    var oauthSource = document.getElementById('oauth-template').innerHTML,
+        oauthTemplate = Handlebars.compile(oauthSource),
+        oauthPlaceholder = document.getElementById('oauth');
+    var params = getHashParams();
 
-        var access_token = params.access_token,
-            refresh_token = params.refresh_token,
-            error = params.error;
+    access_token = params.access_token;
+    refresh_token = params.refresh_token;
+    error = params.error;
 
-        if (error) {
-          alert('There was an error during the authentication');
-        } else {
-          if (access_token) {
-            // render oauth info
-            oauthPlaceholder.innerHTML = oauthTemplate({
-              access_token: access_token,
-              refresh_token: refresh_token
-            });
+    if (error) {
+      alert('There was an error during the authentication');
+    } else {
+      if (access_token) {
+        // render oauth info
+        oauthPlaceholder.innerHTML = oauthTemplate({
+          access_token: access_token,
+          refresh_token: refresh_token
+        });
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+              'Authorization': 'Bearer ' + access_token
+            },
+            success: function(response) {
+              userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+              $('#login').hide();
+              $('#loggedin').show();
+            }
+        });
+      } else {
+          // render initial screen
+          $('#login').show();
+          $('#loggedin').hide();
+      }
+    }
 
-            $.ajax({
-                url: 'https://api.spotify.com/v1/me',
-                headers: {
-                  'Authorization': 'Bearer ' + access_token
-                },
-                success: function(response) {
-                  userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+    if(access_token)
+    {
+      run();
+    }  
+  }
+  
 
-                  $('#login').hide();
-                  $('#loggedin').show();
-                }
-            });
-          } else {
-              // render initial screen
-              $('#login').show();
-              $('#loggedin').hide();
-          }
-        }
+
+  
+  function run()
+  {
+    var dj = new DeeJay(access_token);
+
+    $('#playBtn').click(function() {
+      console.log('play song');
+      dj.play();
+    });
+  
+    $('#pauseBtn').click(function() {
+      console.log('play song');
+      dj.pause();
+    });
+
+    $('#nextBtn').click(function() {
+      console.log('next song');
+      dj.skipSong();
+    });
+
+    $('#analyzeRoomBtn').click(function() {
+      console.log('analyze room');
+      dj.changeGenre();
+    });
+
+    // var device_id = undefined;
+
+    // // https://jmperezperez.com/spotify-web-api-js/
+    // var spotifyApi = new SpotifyWebApi();
+    // spotifyApi.setAccessToken(access_token);
+    //  //spotifyApi.pause(null, callbackTest);
+    // console.log('get my device');
+    // spotifyApi.getMyDevices().then(function(data) {
+    //   console.log('getmydevice - success');
+    //   device_id = data.devices[0].id;
+
+    //   var options = {
+    //     device_id: device_id,
+    //   };
+    //   //spotifyApi.play(options).then(function(data) {
+    //   //  console.log('pause returned')
+    //   //});
+    // }, function(err) {
+    //   console.log('getmydevice - false');
+    //   console.error(err);
+    // });
+  }
+
+  init();
 });
--- source/frontend/public/js/spotifyHelper.js
+++ source/frontend/public/js/spotifyHelper.js
@@ -1,37 +1,55 @@
+var DeeJay = (function(_access_token) { 
+      var self = {};
+      self.access_token = _access_token;
+      self.spotify = new SpotifyWebApi();
+      self.device_id = undefined;
+
+      self.spotify.setAccessToken(self.access_token);
+
+      console.log('get my devices now maaan -- ' + _access_token);
+      self.spotify.getMyDevices().then(function(data) {
+        console.log('getmydevice - success');
+        self.device_id = data.devices[0].id;
+      });
+
+      self.pause = function()
+      {
+        self.spotify.pause({ device_id: self.device_id });
+      }
+
+      self.play = function()
+      {
+        self.spotify.play({ device_id: self.device_id });
+      }
+
+      self.skipSong = function()
+      {
+        self.spotify.skipToNext({ device_id: self.device_id });
+      }
+
+      self.changeGenre = function(current_genre)
+      {
+        if(current_genre === undefined || current_genre === null)
+        {
+            current_genre = 'none';
+        }
+
+        return $.ajax({
+          url: 'http://127.0.0.1:5002/api/genre/next/' + current_genre
+        }).done(function(data) {
+            console.log(data);
+            self.skipSong();
+        });
+      }
+
+      return self;
+  });
+
 $( document ).ready(function() {
-
-    $('#updateSong').click(function() {
-        console.log('requesting what is the nect genre...');
-        
-        getNextGenre().done(function(data) {
-	        $('#cgenre').text(data);
-        })
-
-    });
-
-
-    function obtainNewToken()
-    {
-        var url = '/refresh_token';
-        
-        var data = {
-            'refresh_token': refresh_token
-        };
-
-        makeCall(url, data)
-            .done(function(data) {
-                access_token = data.access_token;
-                oauthPlaceholder.innerHTML = oauthTemplate({
-                    access_token: access_token,
-                    refresh_token: refresh_token
-                });
-            });
-    }
 
     function getNextGenre()
     {
         var url_getNext = 'http://127.0.0.1:5002/api/genre/next/current_genre_is_rock';
-
         return makeCall(url_getNext);
     }
 
